4afe4cac4854e1058d305278dabc2c7735c6dfb2
fernandomurcia1 <fermupa1982@gmail.com>
2019-11-01 10:29:11 -0500

    EjercicioMySql
    Se crea esquema en MySql con el nombre EjercicioMySql se creo una tabla llamada personas con tres campos, Se estudio las sentencia select, update, delete e insert

---
+++ EjercicioMySql.sql
@@ -0,0 +1,15 @@
+/*SENTENCIAS INSERT*/
+insert into persona(nombre, apellido) values ('Luciana', 'Murcia');
+
+insert into persona(nombre, apellido) values ('Karen', 'Martinez');
+
+insert into persona(nombre, apellido) values ('Fernando', 'Murcia');
+
+/*SENTENCIAS SELECT*/
+select * from persona;
+
+/*SENTENCIAS UPDATE*/
+update persona set nombre = 'Lucianita' where idpersona = 1;
+
+/*SENTENCIAS DELETE*/
+delete from persona where id_persona = 3;
