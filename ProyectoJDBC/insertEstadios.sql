260bc549fa2d2586fbc353bfd30f4b528bff8f13
fernandomurcia1 <fermupa1982@gmail.com>
2019-11-06 16:09:37 -0500

    InsertEstadios
    Se realiza los insert para alimentar la tabla de estadios

---
+++ ProyectoJDBC/insertEstadios.sql
@@ -0,0 +1,11 @@
+insert into estadios (Nombre, Ciudad, Capacidad) values('Estadio el campin', 'Bogota', '36000');
+
+insert into estadios (Nombre, Ciudad, Capacidad) values('Estadio Atanasio Girardot', 'Medellin', '44700');
+
+insert into estadios (Nombre, Ciudad, Capacidad) values('Estadio Deportivo cali', 'Cali', '52000');
+
+insert into estadios (Nombre, Ciudad, Capacidad) values('Estadio Roberto Melendez', 'Barranquilla', '46600');
+
+insert into estadios (Nombre, Ciudad, Capacidad) values('Estadio General Santander', 'Cucuta', '30000');
+
+select * from estadios
