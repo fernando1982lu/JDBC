55485f8462e36b7418eb4e3ad672535d44ebc3bb
fernandomurcia1 <fermupa1982@gmail.com>
2019-11-06 16:27:58 -0500

    InsertJugadores
    Se realizan los insert para la tabla jugadores.

---
+++ ProyectoJDBC/InsertJugadores.sql
@@ -0,0 +1,21 @@
+insert into jugadores (Nombre, Apellido_Materno, Apellido_Paterno, Identificacion, Equipo) 
+
+values('Wuilker', 'Faríñez', 'Perez', '56789023', 'Millonarios');
+
+insert into jugadores (Nombre, Apellido_Materno, Apellido_Paterno, Identificacion, Equipo) 
+
+values('Daniel', 'Rosero', 'Valencia','1347859023', 'Cali');
+
+insert into jugadores (Nombre, Apellido_Materno, Apellido_Paterno, Identificacion, Equipo) 
+
+values('Andres', 'Mosquera', 'Marmolejo', '1055689023', 'Medellin');
+
+insert into jugadores (Nombre, Apellido_Materno, Apellido_Paterno, Identificacion, Equipo) 
+
+values('Víctor', 'Cantillo', 'Silva', '1258459023', 'Junior');
+
+insert into jugadores (Nombre, Apellido_Materno, Apellido_Paterno, Identificacion, Equipo) 
+
+values('Matias', 'Pérez', 'García', '1258902343', 'Cucuta');
+
+select * from jugadores
