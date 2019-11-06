8dd9d3f1f1a07e58c01d12d04883c9eb501b565b
fernandomurcia1 <fermupa1982@gmail.com>
2019-11-06 15:48:34 -0500

    InsertEquipos
    Se realiza los insert into en la tabla de equipos

---
+++ ProyectoJDBC/InsertEquipos.sql
@@ -0,0 +1,11 @@
+/*SENTENCIAS INSERT*/
+insert into equipos (Nombre, Ciudad, Direccion, Telefono, Nom_DT) values('Millonarios', 'Bogota', 'Auto Norte 198','4356879', 'Jorgue Pinto');
+
+insert into equipos (Nombre, Ciudad, Direccion, Telefono, Nom_DT) values('Independiente Medellin', 'Medellin', 'Ruta N','5461239', 'Aldo Bobadilla');
+
+insert into equipos (Nombre, Ciudad, Direccion, Telefono, Nom_DT) values('Deportivo Cali', 'Cali', 'AV Jumbo 12 56','4312455', 'Lucas Pusineri');
+
+insert into equipos (Nombre, Ciudad, Direccion, Telefono, Nom_DT) values('Junior', 'Barranquilla', 'Cr 57 72 56','3600338', 'Julio Comesaña');
+
+insert into equipos (Nombre, Ciudad, Direccion, Telefono, Nom_DT) values('Deportivo Cucuta', 'Cucuta', 'Av 2 11 04','46390257', 'Sebastian Ariel');
+
