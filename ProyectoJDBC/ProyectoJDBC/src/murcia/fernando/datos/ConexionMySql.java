1a6de371c598a3d25b9249e4ecf72144b119dc99
fernandomurcia1 <fermupa1982@gmail.com>
2019-11-07 12:11:53 -0500

    ProyectoJDBC
    Se realiza el proyecto final del modulo JDBC en donde se ponen en practica el concepto de pool de conexiones comprobado mediante una sentencia select a una tabla de cada BD (Mysql y Oracle)

--- ProyectoJDBC/ProyectoJDBC/src/murcia/fernando/datos/ConexionMySql.java
+++ ProyectoJDBC/ProyectoJDBC/src/murcia/fernando/datos/ConexionMySql.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package murcia.fernando.datos;
 
+import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 /**
  *
  * @author fernando.murcia
  */
 public class ConexionMySql {
+    
+    //Creamos el metodo statico getDataSuorce no recibe parametros
+    //regresa un tipo dataSource
+    public static DataSource getDataSource(){
+        //Creamos un objeto de tipo BasicDataSource se asigna a una variable
+        //de tipo BasicDataSource
+        BasicDataSource ds = new BasicDataSource();
+        //Se inicializa la propiedad driver classname
+        //Se usa la variable ds y se llama el metodo setDriverClassName
+        //Se proporciona el valor del driver de la base de datos MySql
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        //Colocamos el nombre del usuario y password por medio de los metodos
+        // setusername y setpassword
+        ds.setUsername("root");
+        ds.setPassword("admin");
+        //Colocamos la cadena de conexion a MySql
+        ds.setUrl("jdbc:mysql://localhost:3306/liga?useSSl=false");
+        //Definimos el tamaño del pool de conexiones
+        //Por medio del metodo setInitialSize
+        ds.setInitialSize(5);
+        //Finalmente retornamos el objeto
+        return ds;       
+    }
+    // Creamos metodo getConexion el cual podria arrojar una Excepcion de 
+    // tipo sqlException, es de tipo public static y regresa un objeto de tipo connection
+    public static Connection getConexion() throws SQLException{
+        //Atravez del metodo getDataSource solicitamos el objeto gatConnection
+        return getDataSource().getConnection();
+        //Asi tenemos la configuracion del pool de conexiones de MySql
+    }
     
 }
--- ProyectoJDBC/ProyectoJDBC/src/proyectojdbc/ProyectoJDBC.java
+++ ProyectoJDBC/ProyectoJDBC/src/proyectojdbc/ProyectoJDBC.java
@@ -1,5 +1,11 @@
 package proyectojdbc;
 
+import murcia.fernando.datos.ConexionMySql;
+import murcia.fernando.datos.ConexionOracle;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  *
  * @author fernando.murcia
@@ -9,8 +15,66 @@ public class ProyectoJDBC {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception{
+        //declaramos una variable llamada conn de tipo connection y se inicializa en null
+        Connection conn = null;
+        //Declaramos la variable stmt de tipo Preparedstatement
+        PreparedStatement stmt = null;
+        //Declaramos la variable rs de tipo ResultSet
+        ResultSet rs = null;
+        //Inicializamos un bloque try catch para la ejecucion del codigo
+        try{
+            //primero probamos el pool de MySql ejecutando una consulta
+            /* Por medio de la clase ConexionMySql solicitamos el metodo getConexion
+                y esto nos regresa una conexion a la BD
+            */
+            conn = ConexionMySql.getConexion();
+            System.out.println("Utilizamos el pool de conexiones de MySql");
+            //Creamos el objeto de tipo preparedStatement utilizando una consulta
+            stmt = conn.prepareStatement("SELECT * FROM equipos");
+            //Ejecutamos el query por medio del objeto statemet, nos regresa
+            //el resultSet como resultado de la consulta.
+            rs = stmt.executeQuery();
+            //Mientras tengamos registros mandamos a imprimir las columnas de la tabla
+            while(rs.next()){
+                
+                System.out.println(" " + rs.getInt(1));
+                System.out.println(" " + rs.getString(2));
+                System.out.println(" " + rs.getString(3));
+                System.out.println(" " + rs.getString(4));
+                System.out.println(" " + rs.getString(5));
+                System.out.println(" " + rs.getString(6));
+            }
+            //Cerramos la conexion
+            conn.close();
+            // probamos el pool de oracle por medio de una consulta
+            //Por medio de la clase ConexionOracle solicitamos el metodo getConexion
+            //el cual nos regresa la conexion a oracle
+            conn = ConexionOracle.getConexion();
+            System.out.println("Utilizamos el pool de conexiones de Oracle");
+            //Creamos el objeto de tipo preparedStatement utilizando una consulta
+            //stmt = conn.prepareStatement("SELECT * FROM employees WHERE employee_id in(100, 101, 102)");
+            stmt = conn.prepareStatement("SELECT * FROM job_History ");
+            //Ejecutamos el query por medio del objeto statemet, nos regresa
+            //el resultSet como resultado de la consulta.
+            rs = stmt.executeQuery();
+            //Mientras tengamos registros mandamos a imprimir las columnas de la tabla
+            while(rs.next()){
+                System.out.println(" " + rs.getInt(1));
+                System.out.println(" " + rs.getDate(2));
+                System.out.println(" " + rs.getDate(3));
+                System.out.println(" " + rs.getString(4));
+                System.out.println(" " + rs.getInt(5));
+                
+                
+            }
+            //Cerramos la conexion
+            conn.close();
+          //Finalmente agregamos el catch por si existe alguna excepcion y mandamos imprimir 
+          //la pila de errores
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
     }
     
 }
